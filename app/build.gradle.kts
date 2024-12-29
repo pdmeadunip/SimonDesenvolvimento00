4b5c02e19c6a20a23d0b61f03bbed87cb15fc3ae
profOlavo <itounip@gmail.com>
2024-12-29 19:36:37 -0300

    Exemplo Coroutine implantado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.simondesenvolvimento00"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.simondesenvolvimento00"
--- app/src/main/java/com/example/simondesenvolvimento00/MainActivity.kt
+++ app/src/main/java/com/example/simondesenvolvimento00/MainActivity.kt
@@ -4,14 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.simondesenvolvimento00.ui.theme.SimonDesenvolvimento00Theme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +55,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    jogoGenius()
 }
 
 @Preview(showBackground = true)
@@ -43,5 +63,57 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     SimonDesenvolvimento00Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun jogoGenius(){
+    var texto: String
+    var rodando by remember { mutableStateOf(true) }
+    val game = remember { Game(
+                onAndaPara = { if (rodando) rodando = false}
+            )
+    }
+    if (rodando){
+        LaunchedEffect(Unit) {
+            coroutineScope {
+                launch{game.roda()}
+            }
+        }
+    }
+    Column(
+        modifier = Modifier.background(Color.LightGray),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Spacer(modifier = Modifier.height(50.dp))
+        texto = "Tamanho: ${game.contador}"
+        Text(
+            text = texto, color = Color.Blue, fontSize = 24.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+            )
+        Button(
+            onClick = { rodando = (!rodando)},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Clique")
+        }
+    }
+
+}
+
+class Game(onAndaPara: () -> Unit){
+    var contador by mutableStateOf( 0 )
+    var onAndaPara by mutableStateOf(onAndaPara)
+    private set
+    suspend fun roda() {
+        while (true) {
+            delay(500)
+            contador++
+            if (contador == 10) {
+                onAndaPara()
+                contador = 0
+            }
+        }
     }
 }
